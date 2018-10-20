30893e43b63417bb7a7cae3aa536b0b79fb3baed
Lawrence-Tang <tangzongsheng@gmail.com>
2018-10-20 15:07:14 +0800

    first time commit

---
+++ java/AndroidSDK/BoardType.java
@@ -0,0 +1,64 @@
+package com.friendlyarm.AndroidSDK;
+
+public class BoardType {
+    /* getBoardType return value: */
+    public static final int S3C6410_COMMON = 6410;
+    public static final int S5PV210_COMMON = 210;
+    public static final int S5P4412_COMMON = 4412;
+
+    //s5p4418
+    public static final int S5P4418_BASE = 4418;
+    public static final int NanoPi2 = S5P4418_BASE+0;
+    public static final int NanoPC_T2 = S5P4418_BASE+1;
+    public static final int NanoPi_S2 = S5P4418_BASE+2;
+    public static final int Smart4418 = S5P4418_BASE+3;
+    public static final int NanoPi2_Fire = S5P4418_BASE+4;
+    public static final int NanoPi_M2 = S5P4418_BASE+5;
+    public static final int NanoPi_M2A = S5P4418_BASE+7;
+    public static final int Smart4418SDK = S5P4418_BASE+0x103;
+    public static final int S5P4418_MAX = Smart4418SDK;
+
+    //s5p6818
+    public static final int S5P6818_BASE = 6818;
+    public static final int NanoPC_T3 = S5P6818_BASE+1;
+    public static final int NanoPi_M3B = S5P6818_BASE+2;
+    public static final int Smart6818 = S5P6818_BASE+3;
+    public static final int NanoPC_T3T = S5P6818_BASE+4;
+    public static final int NanoPi_Fire3 = S5P6818_BASE+5;
+    public static final int NanoPi_M3 = S5P6818_BASE+7;
+    public static final int S5P6818_MAX = NanoPi_M3;
+
+    //s3c2451
+    public static final int S3C2451_BASE = 2451;
+    public static final int S3C2451_COMMON = S3C2451_BASE+0;
+
+    //allwinner
+    public static final int ALLWINNER_BASE = 7000;
+    public static final int NanoPi_M1 = ALLWINNER_BASE+1;
+    public static final int NanoPi_NEO = ALLWINNER_BASE+2;
+    public static final int NanoPi_NEO_Air = ALLWINNER_BASE+3;
+    public static final int NanoPi_M1_Plus = ALLWINNER_BASE+4;
+    public static final int NanoPi_A64 = ALLWINNER_BASE+5;
+    public static final int NanoPi_NEO2 = ALLWINNER_BASE+6;
+    public static final int NanoPi_M1_Plus2 = ALLWINNER_BASE+7;
+    public static final int NanoPi_NEO_Plus2 = ALLWINNER_BASE+8;
+    public static final int NanoPi_NEO_Core = ALLWINNER_BASE+9;
+    public static final int NanoPi_Duo = ALLWINNER_BASE+10;
+    public static final int NanoPi_NEO_Core2 = ALLWINNER_BASE+11;
+    public static final int NanoPi_K1 = ALLWINNER_BASE+12;
+    public static final int NanoPi_K1_Plus = ALLWINNER_BASE+13;
+    public static final int NanoPi_Hero = ALLWINNER_BASE+14;
+    public static final int NanoPi_Duo2 = ALLWINNER_BASE+15;
+    public static final int ALLWINNER_MAX = NanoPi_Duo2;
+
+    //amlogic
+    public static final int AMLOGIC_BASE = (8000);
+    public static final int NanoPi_K2 = AMLOGIC_BASE+1;
+
+    //rk3399
+    public static final int RK3399_BASE = (9000);
+    public static final int NanoPC_T4 = RK3399_BASE+1;
+    public static final int NanoPi_M4 = RK3399_BASE+2;
+    public static final int NanoPi_NEO4 = RK3399_BASE+3;
+    public static final int RK3399_MAX = NanoPi_NEO4;
+}
---
+++ java/AndroidSDK/WatchDogEnum.java
@@ -0,0 +1,37 @@
+package com.friendlyarm.AndroidSDK;
+
+public class WatchDogEnum {
+    /* include/uapi/linux/watchdog.h */
+    public final static int WDIOC_GETSUPPORT = 0x80285700;
+    public final static int WDIOC_GETSTATUS = 0x80045701;
+    public final static int WDIOC_GETBOOTSTATUS = 0x80045702;
+    public final static int WDIOC_GETTEMP = 0x80045703;
+    public final static int WDIOC_SETOPTIONS = 0x80045704;
+    public final static int WDIOC_KEEPALIVE = 0x80045705;
+    public final static int WDIOC_SETTIMEOUT = 0xC0045706;
+    public final static int WDIOC_GETTIMEOUT = 0x80045707;
+    public final static int WDIOC_SETPRETIMEOUT = 0xC0045708;
+    public final static int WDIOC_GETPRETIMEOUT = 0x80045709;
+    public final static int WDIOC_GETTIMELEFT = 0x8004570A;
+
+    public final static int WDIOF_UNKNOWN = -1;  /* Unknown flag error */
+    public final static int WDIOS_UNKNOWN = -1;  /* Unknown status error */
+
+    public final static int WDIOF_OVERHEAT = 0x0001;  /* Reset due to CPU overheat */
+    public final static int WDIOF_FANFAULT = 0x0002;  /* Fan failed */
+    public final static int WDIOF_EXTERN1 = 0x0004;  /* External relay 1 */
+    public final static int WDIOF_EXTERN2 = 0x0008;  /* External relay 2 */
+    public final static int WDIOF_POWERUNDER = 0x0010;  /* Power bad/power fault */
+    public final static int WDIOF_CARDRESET = 0x0020;  /* Card previously reset the CPU */
+    public final static int WDIOF_POWEROVER = 0x0040;  /* Power over voltage */
+    public final static int WDIOF_SETTIMEOUT = 0x0080;  /* Set timeout (in seconds) */
+    public final static int WDIOF_MAGICCLOSE = 0x0100;  /* Supports magic close char */
+    public final static int WDIOF_PRETIMEOUT = 0x0200;  /* Pretimeout (in seconds), get/set */
+    public final static int WDIOF_ALARMONLY = 0x0400;  /* Watchdog triggers a management or other external alarm not a reboot */
+    public final static int WDIOF_KEEPALIVEPING = 0x8000;  /* Keep alive ping reply */
+
+    public final static int WDIOS_DISABLECARD = 0x0001;  /* Turn off the watchdog timer */
+    public final static int WDIOS_ENABLECARD = 0x0002;  /* Turn on the watchdog timer */
+    public final static int WDIOS_TEMPPANIC = 0x0004;  /* Kernel panic on temperature trip */
+
+}
